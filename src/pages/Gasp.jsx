0f8c12a21079aaaebfbf4e3b4e46a868384899f5
kimdaes <kimdaes@iu.edu>
2024-03-18 08:32:11 -0400

    added base structure

---
+++ src/pages/Gasp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gasp = () => {
+  return (
+    <div>
+      Gasp
+    </div>
+  )
+}
+
+export default Gasp
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Daesu Kim
+    </div>
+  )
+}
+
+export default Home
