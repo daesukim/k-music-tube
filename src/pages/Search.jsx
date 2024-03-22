77a23fd231ce0a1affdecea680ebcb90a66da570
kimdaes <kimdaes@iu.edu>
2024-03-22 06:53:43 -0400

    using rapid api

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,30 +1,53 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Main from '../components/section/Main'
 
-import { useParams } from 'react-router-dom'
 import VideoSearch from '../components/videos/VideoSearch';
+import {fetchFromAPI} from '../utils/api'
 
 const Search = () => {
   const { searchID }  = useParams();
   const[videos, setVideos] = useState([]);
+  const[nextPageToken, setNextPageToken] = useState(null);
+  const[loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchID}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
-    )
-    .then(response => response.json())
-    .then(result => {
-      console.log(result);
-      setVideos(result.items)
-    })
-    .catch(error => console.log(error));
+    setVideos([]);
+    fetchVideos(searchID);
+    setLoading(true);
   }, [searchID]);
+
+  const fetchVideos = (query, pageToken = '') => {
+    fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+    .then((data) => {
+        setNextPageToken(data.nextPageToken);
+        setVideos((prevVideos) => [...prevVideos, ...data.items]);
+        setLoading(false);
+    })
+    .catch((error) => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+    });
+  };
+
+  const handleLoadMore = () => {
+      if (nextPageToken) {
+          fetchVideos(searchID, nextPageToken);
+      }
+  };
+
+  const searchPageClass = loading ? 'isLoading' : 'isLoaded';
 
   return (
     <Main title = "Search" description = "Welcome to Search">
-      <section id = 'searchPage'>
+      <section id = 'searchPage' className={searchPageClass}>
         <div className='video_inner search'>
           <VideoSearch videos = {videos} />
+        </div>
+        <div className='video_more'>
+          {nextPageToken && (
+            <button onClick={handleLoadMore}> More </button>
+          )}
         </div>
       </section>
     </Main>
