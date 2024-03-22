42d18598a9c785c40dfbfefb96afbc84b65bb84d
kimdaes <kimdaes@iu.edu>
2024-03-22 05:11:54 -0400

    adding contents to search page

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import { useNavigate } from 'react-router-dom'
 
 const Search = () => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    console.log(searchKeyword);
+    if(searchKeyword){
+      navigate(`/search/${searchKeyword}`);
+      setSearchKeyword("");
+    }
+  };
+
+
   return (
     <div id = 'search'>
         <div className='search_inner'>
@@ -9,11 +23,16 @@ const Search = () => {
             </label>
             <input
                 type ="search"
-                name ="searchInput"
                 id = "searchInput"
-                autoComplete="off"
+                placeholder='search'
+                autoComplete='off'
                 className="search_input"
-                placeholder='Search'
+                onChange={e => setSearchKeyword(e.target.value)}
+                onKeyDown={e => {
+                  if (e.key === 'Enter'){
+                    handleSearch();
+                  }
+                }}
             />
         </div>
     </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
 
+import { useParams } from 'react-router-dom'
+import VideoSearch from '../components/videos/VideoSearch';
+
 const Search = () => {
+  const { searchID }  = useParams();
+  const[videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchID}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+    )
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      setVideos(result.items)
+    })
+    .catch(error => console.log(error));
+  }, [searchID]);
+
   return (
     <Main title = "Search" description = "Welcome to Search">
-      Search
+      <section id = 'searchPage'>
+        <div className='video_inner search'>
+          <VideoSearch videos = {videos} />
+        </div>
+      </section>
     </Main>
   )
 }
